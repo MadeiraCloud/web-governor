7c8dad05a815bd852bb411f3c65b99a9b765db08
Paula Meng <0065paula@gmail.com>
2015-03-26 22:44:48 +0800

    [gh-pages] add all animation :girl:

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(window).load(function() {
     // start up after 2sec no matter what
     window.setTimeout(function(){
         $('body').removeClass("loading").addClass('loaded');
-    }, 500);
+    }, 100);
 });
 
 skrollr.init();
