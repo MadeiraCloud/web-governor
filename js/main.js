e4f1f59abc1c06042e9eed51419d72392cd5bcfc
Morris <morris@mc2.io>
2015-03-31 11:36:47 +0800

    Change text content

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ $(window).load(function() {
     $('body').removeClass("loading").addClass('loaded');
   }, 100);
 
-
   // Typing
-  $(".type-rule").t('all <span class="rule-resource">instance.tag</span>=<span class="rule-select">web</span> in <span class="rule-resource">subnet.type</span>=<span class="rule-select">public</span>',{
+  $(".type-rule").t('alert if <span class="rule-resource">instance.tag.role</span>=<span class="rule-select">web</span> and <span class="rule-resource">instance.subnet.type</span>=<span class="rule-select">public</span>',{
     speed_vary : false
     , speed : 90
     , delay : 0.2
